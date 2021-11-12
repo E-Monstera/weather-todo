1c2e238874867d30fd0c7331a97ab184847c8897
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-11-12 15:10:50 -0500

    Updated API url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,8 @@
 import axios from "axios";
 import { authHeader } from './auth-header';
 
-const API_URL = "http://localhost:5000/";
+// const API_URL = "http://localhost:5000/";
+const API_URL = "https://intense-oasis-80724.herokuapp.com/";
 
 // Registers a new user 
 const register = async (newUser) => {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { authHeader } from './auth-header';
-const API_URL = 'http://localhost:5000'
+// const API_URL = 'http://localhost:5000'
+const API_URL = "https://intense-oasis-80724.herokuapp.com/";
 
 const getWeather = async (location) => {
     try {
